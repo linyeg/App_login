e4b012995009d36339f2ecadd0c50daa91f67d9f
linyeg <75706892+linyeg@users.noreply.github.com>
2023-08-30 23:46:56 -0600

    DataSource ListElement

--- lib/infrastructure/datasource/listElement_impl.dart
+++ lib/infrastructure/datasource/listElement_impl.dart
@@ -1,18 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:login/infrastructure/mapper/listElement_mapper.dart';
+import 'package:login/infrastructure/models/listelement_model.dart';
 
 import '../../domain/datasource/listElement_datasource.dart';
 import '../../domain/entities/ListElement_entity.dart';
 import '../mapper/element_mapper.dart';
-import '../models/Element_model.dart';
-import '../models/ListelEments_model.dart';
 
 class ListElementDataSourceAPI implements ListElementsDataSource {
   @override
   Future<ListElementEntity> getListElement() async {
     var dio = await Dio().get(
         'https://notigram.com/wp-json/wp/v2/posts?per_page=10&status=publish&page=1&_embed');
-    return ListElementMapper().toEntityListElement();
+    return ListElementMapper()
+        .toEntityListElement(ListElementModel.fromJson(dio.data));
   }
 
   @override
